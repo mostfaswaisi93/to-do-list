982f6a8a9278bd7ddd88b1f937496c64c17f8864
Mustafa Al-Swaisi <mostfaswaisi93@gmail.com>
2021-04-01 10:04:28 +0300

    To-Do List

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 // Main JS
+// Click On Toggle
+let toggle = document.querySelector('.toggle');
+let div = document.querySelector('.form');
+
+toggle.addEventListener('click', () => {
+    if (div.style.display === "none") {
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+});
